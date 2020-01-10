fc1e08095e7912e1c274c59015124d28ca10ab6c
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-10 17:20:36 +0300

    create database

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlachemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -10,24 +10,27 @@ app.config["SECRET_KEY"] = "6358c61aaba8619f201e9b2772c66d75"
 db = SQLAlchemy(app)
 
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.column(db.String(20), unique=True, nullable =False)
-    email = db.column(db.String(120), unique=True, nullable =False)
-    image_file = db.column(db.String(20), nullable =False, default= "default.jpg")
-    password = db.Column(db.String(60),nullable = False)
-    
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref="author",lazy=True)
+
     def __repr__(self):
-        return f"User('{self.username}','{self.email}','{self.image_file}')"
-    
-class Posts(db.model):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable = False)
-    date_posted = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"), nullable = False)
+
     def __repr__(self):
-        return f"Post('{self.title}','{self.date_posted}')"
+        return f"Post('{self.title}', '{self.date_posted}')"
     
 
 posts = [
