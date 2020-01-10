e4eddcb49756609ef6f0174566f4fae9eed038ab
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-10 13:11:16 +0300

    add forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm,LoginForm
+
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "e4f5574dafdc479321bb1c66c18147f577f3f452"
 
 posts = [
     {
@@ -17,22 +21,27 @@ posts = [
 ]
 
 
-
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html", posts= posts,title = "Home page")
+    return render_template("index.html", posts=posts, title="Home page")
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title = "About")
+
+
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template("login.html", title = "Login", form=form)
+
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    form = RegistrationForm()
+    return render_template("register.html", form = form, title = "Register")
 
 
 if __name__ == "__main__":
